65924ae318b16fe59408e424fe3a79eadf694fda
krishna chaithanya <krishnachaithanya0202@gmail.com>
2026-04-17 10:58:55 +0530

    Add files via upload

--- EMBEDDED C/pgm2.c
+++ EMBEDDED C/pgm2.c
@@ -1,16 +1,27 @@
-#include <reg52.h>
+#include <reg51.h>
 
-sbit LED = P1^0;
-sbit SW = P2^0;
-
-void main() 
-{
-	while(1) {
-	if (SW==0) {
-		LED = 0;
-	}
-	else {
-		LED = 1;
-	}
+void delay(unsigned int count) {   //delay called
+    unsigned int i, j;
+    i = 0;
+    while(i < count) {
+        j = 0;
+        while(j < 500) {
+            j++;
+        }
+        i++;
+    }
 }
+
+void main() {
+    unsigned char pattern;
+    
+    while(1) { //infinite loop
+        pattern = 0x01; // Start with 0000 0001
+        
+        while(pattern != 0x00) {
+            P1 = pattern;      // Output to Port 1
+            delay(100);        // Wait
+            pattern = pattern << 1; // Left shift
+        }
+    }
 }
