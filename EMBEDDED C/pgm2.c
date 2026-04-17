59b15c14c8498081f73285926df11510027e4284
krishna chaithanya <krishnachaithanya0202@gmail.com>
2026-04-17 10:57:38 +0530

    Add files via upload

--- EMBEDDED C/pgm2.c
+++ EMBEDDED C/pgm2.c
@@ -1,7 +1,18 @@
 #include <reg52.h>
 
+void delay() {
+    unsigned int i, j;
+    for(i = 0; i < 200; i++)
+        for(j = 0; j < 100; j++);
+}
+
 void main() {
-	P1 =0x00;
-	while (1);
-	
+    while(1) {
+        
+        P1 = 0x55;   // 01010101 ? Even LEDs ON
+        delay();
+        
+        P1 = 0xAA;   // 10101010 ? Odd LEDs ON
+        delay();
+    }
 }
