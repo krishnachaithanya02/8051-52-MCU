56287097ab01759a4fa07c172ed270fd372283ad
krishna chaithanya <krishnachaithanya0202@gmail.com>
2026-04-27 14:44:18 +0530

    Create LCD display.c

---
+++ EMBEDDED C/TIMERS/LCD display.c
@@ -0,0 +1,78 @@
+#include <reg51.h>
+#define port2 P2
+
+sbit RS = P3^0;
+sbit RW = P3^1;
+sbit EN = P3^2;
+
+
+void delay(unsigned int time);
+void lcd_cmd(unsigned char cmd);
+void lcd_data(unsigned char d);
+void lcd_init();
+void lcd_string(char *str);
+
+void main() {
+        lcd_init();         
+    
+    // Command to set cursor to first line
+         
+    
+    // Function call to display string
+    lcd_string("HELLO"); 
+
+    
+    while(1) {
+        // Controller stays here after execution
+    }
+}
+
+
+void lcd_init() {
+    lcd_cmd(0x38); // 8-bit mode, 2 lines
+    lcd_cmd(0x0C); // Display ON, Cursor OFF
+    lcd_cmd(0x01); // Clear Display RAM
+    delay(2);
+}
+
+void lcd_string(char *str) {
+    
+    while(*str != '\0') {
+        lcd_data(*str);
+        str++;
+    }
+}
+
+void lcd_cmd(unsigned char cmd) {
+    port2 = cmd;
+    RS = 0; // Command Register
+    RW = 0; // Write Mode
+    EN = 1;
+    delay(1);
+    EN = 0;
+}
+
+void lcd_data(unsigned char d) {
+    port2 = d;
+    RS = 1; // Data Register
+    RW = 0; // Write Mode
+    EN = 1;
+    delay(1);
+    EN = 0;
+}
+
+void delay(unsigned int time) {
+    unsigned int i;
+    // Standard nested for loops for delay
+    for(i = 0; i < time; i++) {
+       TMOD = 0xF0;
+			 TMOD = 0x01;
+			 TH0 = 0xFC;
+			 TL0 = 0x66;
+			 TR0 = 1;
+			 while(TF0==0);
+			  TR0=0;
+			  TF0=0;
+			
+    }
+}
